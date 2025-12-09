b544c0f0a50ecc394f9ee99b3128ab502e95d71a
Ravi Tiwari <raviashoktiwari9559@gmail.com>
2025-12-09 12:23:33 +0530

    edit paths

--- src/components/layout/AdminLayout.jsx
+++ src/components/layout/AdminLayout.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 // 🔥 FIX: Import path correct kiya aur Alias (@) use kiya
-import Navbar from "@/components/common/Navbar";
-import Footer from "@/components/common/Footer";
+import Navbar from "../common/NavBar.jsx";
+import Footer from "../common/Footer";
 import AdminSidebar from "../../Pages/AdminSideBar.jsx";
 
 const AdminLayout = () => {
